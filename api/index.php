6307345dc797d8ab26e95e69abc17b7644ce881f
iafever <43429375+iafever@users.noreply.github.com>
2024-07-04 19:51:40 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,10 +1,4 @@
 <?php
-  $name = '';
-  if($_GET['name'] != ''){
-    $name = $_GET['name'];
-    echo $name.' Run on vercel.com!<br/>';
-  }else{
-    echo 'Run on vercel.com!<br/>';
-  }
+  echo 'Run on vercel.com!<br/>';
   phpinfo();
 ?>
